85b2714e7166c25f2af2786af431e360b9d04218
kmaho17 <kirkmahoney17@gmail.com>
2025-01-04 23:30:44 -0500

    Update nidoranm.asm

--- data/baseStats/nidoranm.asm
+++ data/baseStats/nidoranm.asm
@@ -1,8 +1,8 @@
 db DEX_NIDORAN_M ; pokedex id
-db 46 ; base hp
-db 57 ; base attack
-db 40 ; base defense
-db 50 ; base speed
+db 55 ; base hp
+db 47 ; base attack
+db 52 ; base defense
+db 41 ; base speed
 db 40 ; base special
 db POISON ; species type 1
 db POISON ; species type 2
